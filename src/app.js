08ab5a64d35a2af3f50b21c0d5c33019bb2fdff9
jucora <artjulius@hotmail.com>
2020-05-14 11:06:43 -0500

    adding functionality to go back home once the new project is created

--- src/app.js
+++ src/app.js
@@ -1,5 +1,33 @@
 import './css/style.css';
 
+const home = () =>{
+    /* ASIDE */
+    let aside = document.createElement("aside");
+    let title = document.createElement("h2");
+    title.innerText = "Projects";
+    let img = document.createElement("img");
+    img.setAttribute("src", "../src/images/add.svg");
+    img.setAttribute("alt", "add-icon");
+    img.setAttribute("id", "add-icon");
+    title.appendChild(img);
+    let ul = document.createElement("ul");
+    ul.setAttribute("id", "projectsList");
+    ul.innerHTML += "<li>Default</li>";
+    aside.appendChild(title);
+    aside.appendChild(ul);
+    document.querySelector(".content").appendChild(aside);
+
+    /* SECTION */
+    let section = document.createElement("section");
+    let title2 = document.createElement("h2");
+    title2.innerText = "TO-DO LIST";
+    section.appendChild(title2)
+    document.querySelector(".content").appendChild(section);
+
+    /*LISTENERS*/
+    document.querySelector("#add-icon").addEventListener("click", createTaskForm);
+}
+
 const projects = (() =>{
     const current = [];
     return{ current }
@@ -9,11 +37,18 @@ const getLocalstorage = () => {
     const currentProjects = JSON.parse(localStorage.getItem('myProjects'));
     return currentProjects;
   }
+  const createContentTag = () =>{
+      document.querySelector("body").innerHTML = "";
+      const content = document.createElement("div");
+      content.classList.add("content");
+      document.querySelector("body").appendChild(content);
+  }
 
 const saveLocalstorage = (newProject) => {
     projects.current.push(newProject)
     localStorage.setItem('myProjects', JSON.stringify(projects));
-    getLocalstorage();
+    createContentTag()
+    home();
   }
 
 const validation = (title, description) =>{
@@ -75,37 +110,6 @@ const createTaskForm = () => {
     validation(title,description)
 })
 }
-
-const home = () =>{
-    /* ASIDE */
-    let aside = document.createElement("aside");
-    let title = document.createElement("h2");
-    title.innerText = "Projects";
-    let img = document.createElement("img");
-    img.setAttribute("src", "../src/images/add.svg");
-    img.setAttribute("alt", "add-icon");
-    img.setAttribute("id", "add-icon");
-    title.appendChild(img);
-    let ul = document.createElement("ul");
-    ul.setAttribute("id", "projectsList");
-    ul.innerHTML += "<li>Default</li>";
-    aside.appendChild(title);
-    aside.appendChild(ul);
-    document.querySelector(".content").appendChild(aside);
-
-    /* SECTION */
-    let section = document.createElement("section");
-    let title2 = document.createElement("h2");
-    title2.innerText = "TO-DO LIST";
-    section.appendChild(title2)
-    document.querySelector(".content").appendChild(section);
-
-    /*LISTENERS*/
-    document.querySelector("#add-icon").addEventListener("click", createTaskForm);
-}
-
-
-
 document.addEventListener(
     'DOMContentLoaded', home()
   );
